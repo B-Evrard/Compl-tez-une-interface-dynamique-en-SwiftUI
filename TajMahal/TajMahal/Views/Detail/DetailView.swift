4f4ef6c811f4196fb5ae6f539a6d550926f8f28b
Bruno Evrard <bruno.evrard@gmail.com>
2024-08-13 10:26:34 +0200

    Vue détails et amèliorations vue Menu

---
+++ TajMahal/TajMahal/Views/Detail/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  TajMahal
+//
+//  Created by Bruno Evrard on 08/08/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ TajMahal/TajMahal/Views/Menu/MenuViewLine.swift
@@ -0,0 +1,18 @@
+//
+//  MenuViewLine.swift
+//  TajMahal
+//
+//  Created by Bruno Evrard on 01/08/2024.
+//
+
+import SwiftUI
+
+struct MenuViewLine: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuViewLine()
+}
