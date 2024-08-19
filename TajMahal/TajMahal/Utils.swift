25f4ac489606168efa624e30e365a25d33922d0d
Bruno Evrard <bruno.evrard@gmail.com>
2024-08-19 09:04:47 +0200

    Mise en forme code  - Correctifs

---
+++ TajMahal/TajMahal/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  TajMahal
+//
+//  Created by Bruno Evrard on 14/08/2024.
+//
+
+import Foundation
---
+++ TajMahal/TajMahal/ViewModel/WelcomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WelcomeViewModel.swift
+//  TajMahal
+//
+//  Created by Bruno Evrard on 15/08/2024.
+//
+
+import Foundation
